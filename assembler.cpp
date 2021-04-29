581077a794bde18bf503dd2c4b739a4be80ed06c
Harsh <patelharsh0603@gmail.com>
2021-04-29 22:39:14 +0530

    Added the function to get the column from line of input1

--- assembler.cpp
+++ assembler.cpp
@@ -1,8 +1,51 @@
 #include<iostream>
 #include<fstream>
+#include<vector>
+#include<string>
 using namespace std;
+
+void onePassScan();
 
 int main()
 {
+    onePassScan();
     return 0;
+}
+
+void onePassScan()
+{
+    int loc;
+    string line, col1, col2, col3;
+    ifstream file("input1.txt");
+    
+    if(file.is_open())
+    {
+
+        int tabPos[2];
+        while(getline(file, line))
+        {
+            bool firstTab = true;
+            for(int i = 0; i<line.size(); i++)
+            {
+                if(line[i] == '\t' && firstTab == true)
+                {
+                firstTab = false;
+                tabPos[0] = i;
+                }
+                else if(line[i] == '\t')
+                    tabPos[1] = i;
+            }
+
+            col1 = line.substr(0,tabPos[0]);
+            col2 = line.substr(tabPos[0] + 1, tabPos[1] - tabPos[0] - 1);
+            col3 = line.substr(tabPos[1] + 1, line.size() - tabPos[1]);
+            cout<<col1<<col2<<col3<<endl;
+        }
+
+        file.close();
+    }
+
+
+
+    
 }
